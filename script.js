5dc8471a28209a99d6933fe5ca418a1664fb6494
Karlo <karlocehlic@gmail.com>
2024-08-09 00:05:08 +0200

    Remove test default books

--- script.js
+++ script.js
@@ -52,16 +52,6 @@ function addBookToLibrary(book) {
     table.appendChild(row)
 }
 
-let book1 = new Book("Lorem", "John", 152, false)
-let book2 = new Book("Ipusum", "Jack", 125, true)
-let book3 = new Book("Dolor", "Jacob", 521, false)
-let book4 = new Book("Sit", "Jerry", 251, false)
-
-addBookToLibrary(book1)
-addBookToLibrary(book2)
-addBookToLibrary(book3)
-addBookToLibrary(book4)
-
 const btn = document.querySelector(".show");
 const formDiv = document.querySelector(".form");
 
