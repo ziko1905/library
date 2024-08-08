d391703e999372753f1db8d81dbc7e212c8ceabd
Karlo <karlocehlic@gmail.com>
2024-08-08 02:15:38 +0200

    Create libray array and book object constructor

---
+++ script.js
@@ -0,0 +1,9 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
