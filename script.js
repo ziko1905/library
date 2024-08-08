fd71978b6f6784971d86b13195b144b877bc7be4
Karlo <karlocehlic@gmail.com>
2024-08-08 02:18:12 +0200

    Add some dummy books for display

--- script.js
+++ script.js
@@ -13,4 +13,14 @@ function addBookToLibrary(book) {
     for (let n = 0; n < myLibrary.length; n++) {
       console.log(`${n + 1} ${myLibrary[n].title} by ${myLibrary[n].author} has ${myLibrary[n].pages} pages`)
     }
-  }
+}
+
+let book1 = new Book("Lorem", "John", 152, false)
+let book2 = new Book("Ipusum", "Jack", 125, true)
+let book3 = new Book("Dolor", "Jacob", 521, false)
+let book4 = new Book("Sit", "Jerry", 251, false)
+
+addBookToLibrary(book1)
+addBookToLibrary(book2)
+addBookToLibrary(book3)
+addBookToLibrary(book4)
