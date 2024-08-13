8a9ff3fd18d94362fcf4352cf0cafa4405e55fdb
Karlo Cehulic <karlocehlic@gmail.com>
2024-08-13 23:07:03 +0200

    Change 'Book' to be a class

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+class Book {
   // the constructor...
+  constructor(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-
+  }
 }
 
 function addBookToLibrary(book) {
